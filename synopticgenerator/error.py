9fb64575cf213a4c4d97817078f240379d6e5102
yamahigashi <yamahigashi+github@gmail.com>
2016-12-09 14:22:09 +0900

    bump year

--- synopticgenerator/error.py
+++ synopticgenerator/error.py
@@ -1,5 +1,5 @@
 """ coding: utf-8 """
-# Copyright 2014, MATSUMOTO Takayoshi
+# Copyright 2016, MATSUMOTO Takayoshi
 # All rights reserved.
 #
 ##############################################################################
--- synopticgenerator/recognizer/image_to_regions.py
+++ synopticgenerator/recognizer/image_to_regions.py
@@ -1,5 +1,5 @@
 ﻿""" coding: utf-8 """
-# Copyright 2014, MATSUMOTO Takayoshi
+# Copyright 2016, MATSUMOTO Takayoshi
 # All rights reserved.
 #
 ##############################################################################
