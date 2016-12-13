8e1cfca84a4a6216f14e95eca59d30971b437b28
yamahigashi <yamahigashi+github@gmail.com>
2016-12-13 18:17:08 +0900

    add new util simply copy file

---
+++ synopticgenerator/utility/copy_file.py
@@ -0,0 +1,25 @@
+""" coding: utf-8 """
+
+import shutil
+import logging
+
+
+class FileRemover(object):
+
+    def __init__(self, config, environ):
+        self.config = config
+        self.environ = environ
+        self.files = config.setdefault("file", "")
+
+    def execute(self, content):
+        src = self.config["src"]
+        dst = self.config["dst"]
+
+        logging.info("copying file from {} to {}".format(src, dst))
+        shutil.copy(src, dst)
+
+        return content
+
+
+def create(config, environ):
+    return FileRemover(config, environ)
