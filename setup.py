4def2a18fb4d1c69c483719e253a0f8c8acd2242
yamahigashi <yamahigashi+github@gmail.com>
2016-12-14 14:32:31 +0900

    add setup scripts

---
+++ setup.py
@@ -0,0 +1,14 @@
+﻿from distutils.core import setup
+setup(
+    name = "synopticgenerator",
+    packages = ["synopticgenerator"],
+    version = "1.0.0",
+    description = "Synoptic generator for DCC tools",
+    author = "MATSUMOTO Takayoshi",
+    author_email = "yamahigashi@gmail.com",
+    url = "https://github.com/yamahigashi/SynopticGenerator",
+    keywords = ["vfx", "dcc"],
+    classifiers = [
+        ],
+    long_description = """ """
+)
