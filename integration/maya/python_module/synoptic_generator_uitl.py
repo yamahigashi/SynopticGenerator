ee18883ab6410445ee078976931cb64bf8bd3398
yamahigashi <yamahigashi+github@gmail.com>
2017-01-17 19:00:48 +0900

    fixed maya isolate and capture snippet issue

--- integration/maya/python_module/synoptic_generator_uitl.py
+++ integration/maya/python_module/synoptic_generator_uitl.py
@@ -30,30 +30,58 @@ def set_child_visibility(parent, flag):
         if flag:
             print "show", kid
             cmds.showHidden(kid)
+
         else:
             print "hide", kid
             if cmds.getAttr("{}.visibility".format(kid)):
                 restore.append(kid)
+
             cmds.hide(kid)
 
     return restore
 
 
+def get_panels():
+    # type: () -> [str]
+
+    return cmds.getPanel(typ="modelPanel")
+
+
+def isolate_execute(func):
+    # type: (str, function) -> None
+
+    def _iso(panel):
+        cmds.isolateSelect(panel, removeSelected=True)
+        cmds.isolateSelect(panel, addSelected=True)
+        cmds.isolateSelect(panel, update=True)
+        cmds.isolateSelect(panel, state=1)
+        cmds.select(clear=True)
+
+        func()
+
+        cmds.isolateSelect(panel, removeSelected=True)
+
+    for panel in get_panels():
+        _iso(panel)
+
+
 def capture_selection(basedir, width, height):
+    # type: (str, int, int) -> None
+
     selection = cmds.ls(sl=True)
     cmds.select(clear=True)
+
     for s in selection:
         cmds.select(s)
         kids_restore = set_child_visibility(s, False)
-        cmds.isolateSelect("modelPanel4", addSelected=True)
-        cmds.isolateSelect("modelPanel4", update=True)
-        cmds.isolateSelect("modelPanel4", state=1)
-        cmds.select(clear=True)
-        fname = "D:\\{}".format(s)
-        cap(fname, width, height)
-        cmds.showHidden(kids_restore)
-        cmds.select(s)
-        cmds.isolateSelect("modelPanel4", removeSelected=True)
+
+        def _inner():
+            fname = "D:\\{}".format(s)
+            cap(fname, width, height)
+            cmds.showHidden(kids_restore)
+            cmds.select(s)
+
+        isolate_execute(_inner)
         # cmds.select(clear=True)
 
     cmds.isolateSelect("modelPanel4", state=0)
